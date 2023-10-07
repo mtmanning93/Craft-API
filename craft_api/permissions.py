8093878f4dab2dd950aba6cdf36b284b901fe3f7
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-07 14:43:16 +0000

    Add doctring

--- craft_api/permissions.py
+++ craft_api/permissions.py
@@ -2,6 +2,9 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Checks to see if the user is the object owner.
+    """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
