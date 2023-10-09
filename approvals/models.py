77ec2f47c4a3180e7feb645d76e0190b01fa8b04
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-09 10:11:59 +0000

    Create approval model

--- approvals/models.py
+++ approvals/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from profiles.models import Profile
 
-# Create your models here.
+
+class Approval(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+        unique_together = ['owner', 'profile']
+
+    def __str__(self):
+        return f"{self.owner}, {self.profile}"
