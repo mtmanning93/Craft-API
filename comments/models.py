2e62f701a5a0d5ffa886a493b63e12d31f118ff9
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-09 08:03:51 +0000

    Create Comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model, related to 'owner' via the User FK and
+    'post' via the Post FK.
+    Ordering set to '-created_on' to see the newest comment first,
+    like a text conversation.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.content
