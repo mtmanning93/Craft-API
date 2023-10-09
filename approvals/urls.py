848354403b508207e59fece854a84c8ec50f1b6f
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-09 10:23:52 +0000

    Add approval list and details views, add urls

---
+++ approvals/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from approvals import views
+
+urlpatterns = [
+    path('approvals/', views.ApprovalList.as_view()),
+    path('approvals/<int:pk>/', views.ApprovalDetail.as_view()),
+]
--- approvals/views.py
+++ approvals/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from craft_api.permissions import IsOwnerOrReadOnly
+from .models import Approval
+from .serializers import ApprovalSerializer
 
-# Create your views here.
+
+class ApprovalList(generics.ListCreateAPIView):
+    """
+    List and create approvals when logged in.
+    """
+    serializer_class = ApprovalSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Approval.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ApprovalDetail(generics.RetrieveDestroyAPIView):
+    """
+    Get an approval's details and delete it if owner by user and logged in.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ApprovalSerializer
+    queryset = Approval.objects.all()
