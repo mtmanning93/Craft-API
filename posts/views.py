e95d937f91c1131567f558ba178ce5b86197175c
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-10 05:04:25 +0000

    Add filter method to posts app

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Post
 from .serializers import PostSerializer
 from craft_api.permissions import IsOwnerOrReadOnly
@@ -19,6 +20,7 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend,
     ]
     ordering_fields = [
         'comments_count',
@@ -28,6 +30,14 @@ class PostList(generics.ListCreateAPIView):
     search_fields = [
         'owner__username',
         'title',
+    ]
+    filterset_fields = [
+        # user feed
+        'owner__followed__owner__profile',
+        # user liked posts
+        'like__owner__profile',
+        # user posts
+        'owner__profile',
     ]
 
     def perform_create(self, serializer):
