b32d036611ead18e1a066c43d07af46c0f296437
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-11 10:58:03 +0000

    Add filter fields to ApprovalList

--- approvals/views.py
+++ approvals/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from craft_api.permissions import IsOwnerOrReadOnly
 from .models import Approval
 from .serializers import ApprovalSerializer
@@ -10,7 +11,24 @@ class ApprovalList(generics.ListCreateAPIView):
     """
     serializer_class = ApprovalSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Approval.objects.all()
+    queryset = Approval.objects.all().order_by('-created_on')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    ordering_fields = [
+        'owner__username',
+        'profile',
+        'created_on',
+    ]
+    search_fields = [
+        'profile__owner__username',
+    ]
+    filterset_fields = [
+        'profile__owner',
+        'owner',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
