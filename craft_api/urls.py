57eddf7e0194d8c4665820cc651fb2d246d63e67
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-12 07:31:43 +0000

    Create craft-api serializer, and update settings with rest_auth_serializers

--- craft_api/urls.py
+++ craft_api/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
-        'dj-rest-auth/registration/', include('dj_rest_Auth.registration.urls')
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
     path('', include('profiles.urls')),
     path('', include('companies.urls')),
