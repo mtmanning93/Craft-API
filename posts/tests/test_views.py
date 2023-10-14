37909d314c55b7b093d5a796032563f9ae2da142
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-14 06:20:58 +0000

    Add simple tests for post details view

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -7,14 +7,14 @@ from ..models import Post
 from ..serializers import PostSerializer
 
 
-class PostListAPITest(TestCase):
+class PostListViewTest(TestCase):
     """
     Tests for the PostList view,
     including listing and creating.
     """
     def setUp(self):
         """
-        Set up test object instances
+        Set up test object instances.
         """
         self.user = User.objects.create_user(
             username='testuser',
@@ -52,9 +52,77 @@ class PostListAPITest(TestCase):
         """
         url = reverse('post-list')
         client = APIClient()
-        # Authenticate the client with a user
+
         client.force_authenticate(user=self.user)
         response = client.post(url, self.post_data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Post.objects.count(), 2)
+
+
+class PostDetailViewTest(TestCase):
+
+    def setUp(self):
+        """
+        Set up test data.
+        """
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+            )
+        self.post = Post.objects.create(
+            owner=self.user,
+            title='Test Post',
+            content='This is a test post'
+            )
+
+    def test_retrieve_post(self):
+        """
+        Checks the retrieval of a specific post instance is successful.
+        """
+        url = reverse('post-detail', kwargs={'pk': self.post.pk})
+        client = APIClient()
+        response = client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_post(self):
+        """
+        Checks that an authenticated user can update a post.
+        First checks the original data, then the updated post data.
+        """
+        self.assertEqual(self.post.title, 'Test Post')
+        self.assertEqual(self.post.content, 'This is a test post')
+
+        update_data = {
+            'title': 'Updated Post',
+            'content': 'This post has been updated'
+            }
+
+        url = reverse('post-detail', kwargs={'pk': self.post.pk})
+        client = APIClient()
+
+        client.force_authenticate(user=self.user)
+
+        response = client.put(
+            url,
+            update_data,
+            format='json'
+            )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.post.refresh_from_db()
+        self.assertEqual(self.post.title, 'Updated Post')
+
+    def test_delete_post(self):
+        """
+        Tests to see if an authenticated user can also delete a post.
+        """
+        url = reverse('post-detail', kwargs={'pk': self.post.pk})
+        client = APIClient()
+
+        client.force_authenticate(user=self.user)
+        response = client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Post.objects.count(), 0)
