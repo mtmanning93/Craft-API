dd5531b8501b3e910818107db42290fd1bf97d88
Matt Manning <mtmanning93@hotmail.co.uk>
2023-11-05 13:21:51 +0000

    Return to previous state

--- profiles/views.py
+++ profiles/views.py
@@ -47,7 +47,7 @@ class ProfileList(generics.ListAPIView):
     ]
 
 
-class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     Allows the retrieval of a profile and the ability to
     edit it if the user is the owner.
