e32b9c709c7de70c03f35e7d60c01e63d17faa18
Matt Manning <mtmanning93@hotmail.co.uk>
2023-11-06 16:48:40 +0000

    Comment out all delete, logout functionality

--- profiles/views.py
+++ profiles/views.py
@@ -61,16 +61,16 @@ class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
         approval_count=Count('approval__owner', distinct=True),
     ).order_by('-created_on')
 
-    def destroy(self, request, *args, **kwargs):
-        profile = self.get_object()
+    # def destroy(self, request, *args, **kwargs):
+    #     profile = self.get_object()
 
-        if not request.user == profile.owner:
-            return Response(
-                {"detail": "You do not have permission to perform this action."},
-                status=status.HTTP_403_FORBIDDEN,
-            )
+    #     if not request.user == profile.owner:
+    #         return Response(
+    #             {"detail": "You do not have permission to perform this action."},
+    #             status=status.HTTP_403_FORBIDDEN,
+    #         )
 
-        # Log the user out and delete the associated user and profile
-        logout_and_delete_route(request, profile)
+    #     # Log the user out and delete the associated user and profile
+    #     logout_and_delete_route(request, profile)
 
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
