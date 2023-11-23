a3629688d00936842a6b574ae86f090b2e78a43e
Matt Manning <mtmanning93@hotmail.co.uk>
2023-11-23 12:22:05 +0000

    Test the new delete account method

--- craft_api/views.py
+++ craft_api/views.py
@@ -6,52 +6,6 @@ from .settings import (
     JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE,
 )
-# from profiles.models import Profile
-# from django.contrib.auth.models import User
-
-# @api_view(["DELETE"])
-# def delete_account(request, pk):
-#     # Check if the user making the request is the same as the one to be deleted
-#     if request.user.id != int(pk):
-#         return Response({"error": "You do not have permission to delete this user."}, status=status.HTTP_403_FORBIDDEN)
-
-#     try:
-#         # Get the user and profile with the specified pk
-#         user = User.objects.get(pk=pk)
-#         profile = Profile.objects.get(owner=user)
-
-#         # Delete the user and profile
-#         user.delete()
-#         profile.delete()
-
-#         # Logout the user by clearing cookies
-#         response = Response({"result": "User and profile deleted."}, status=status.HTTP_200_OK)
-#         response.set_cookie(
-#             key=JWT_AUTH_COOKIE,
-#             value="",
-#             httponly=True,
-#             expires="Thu, 01 Jan 1970 00:00:00 GMT",
-#             max_age=0,
-#             samesite=JWT_AUTH_SAMESITE,
-#             secure=JWT_AUTH_SECURE,
-#         )
-#         response.set_cookie(
-#             key=JWT_AUTH_REFRESH_COOKIE,
-#             value="",
-#             httponly=True,
-#             expires="Thu, 01 Jan 1970 00:00:00 GMT",
-#             max_age=0,
-#             samesite=JWT_AUTH_SAMESITE,
-#             secure=JWT_AUTH_SECURE,
-#         )
-#         return response
-
-#     except User.DoesNotExist:
-#         raise Http404("User not found.")
-#     except Profile.DoesNotExist:
-#         raise Http404("Profile not found.")
-#     except Exception as e:
-#         return JsonResponse({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view()
--- profiles/views.py
+++ profiles/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from craft_api.permissions import IsOwnerOrReadOnly
 from rest_framework.views import APIView
 from django.http import Http404
+from rest_framework.generics import get_object_or_404
 from craft_api.views import logout_route
 from django.contrib.auth.models import User
 
@@ -71,18 +72,66 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
 class DeleteAccount(APIView):
     permission_classes = [IsAuthenticated]
 
-    def delete(self, request, *args, **kwargs):
-        user = self.request.user
+    def delete(self, request, pk):
+        user = request.user
+        print(user.username)
+        profile = Profile.objects.filter(
+            owner=user
+        ).first()
+        print(profile)
 
-        # Check if the user has a profile
-        try:
-            profile = Profile.objects.get(owner=user)
-        except Profile.DoesNotExist:
-            raise Http404("Profile not found for the user.")
+        profile_to_del = Profile.objects.filter(
+            pk=pk
+        ).first()
+        print(profile_to_del)
 
-        # Delete the user and associated profile
-        user.delete()
-        profile.delete()
-        logout_route(request._request)
+        user_to_del = profile_to_del.owner
+        print(user_to_del)
 
-        return Response({"result": "User and profile deleted."}, status=status.HTTP_200_OK)
+        if user_to_del is None or profile_to_del is None:
+            raise Http404
+
+        if user == user_to_del:
+            user_to_del.is_active = False 
+            try:
+                user_to_del.save()
+            except Exception as e:
+                return Response(
+                    str(e),
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+            
+            profile_to_del.delete()
+            return Response(
+                {'detail': 'The user account has been successfully deleted.'},
+                status=status.HTTP_200_OK
+            )
+
+            logout_route(request._request)
+        
+        # If the user got this far, they don't have permission.
+        return Response(
+            {"detail": "You are not allowed to perform this action."},
+            status=status.HTTP_403_FORBIDDEN
+        )
+
+
+# # WORKS BUT MUST TEST // Doesnt work with logout route
+# class DeleteAccount(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def delete(self, request, *args, **kwargs):
+#         user = self.request.user
+
+#         # Check if the user has a profile
+#         try:
+#             profile = Profile.objects.get(owner=user)
+#         except Profile.DoesNotExist:
+#             raise Http404("Profile not found for the user.")
+
+#         # Delete the user and associated profile
+#         user.delete()
+#         profile.delete()
+#         # logout_route(request._request)
+
+#         return Response({"result": "User and profile deleted."}, status=status.HTTP_200_OK)
